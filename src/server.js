1ece9d37a4bfdff6d4ff97afc8aa8b04b00c78f2
iinsue <hinsu@rmsoft.kr>
2023-06-21 23:12:06 +0900

    cors 추가

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import api from "./api/api";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["POST", "PUT", "GET", "DELETE"],
+  })
+);
 app.use(cookieParser());
 
 app.use("/api", api);
